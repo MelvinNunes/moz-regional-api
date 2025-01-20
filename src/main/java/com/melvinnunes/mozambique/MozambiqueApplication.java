51380e39b6751a783f19383f67471a629bb85175
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-20 21:47:06 +0200

    init: starting the project

---
+++ src/main/java/com/melvinnunes/mozambique/MozambiqueApplication.java
@@ -0,0 +1,13 @@
+package com.melvinnunes.mozambique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MozambiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MozambiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melvinnunes/mozambique/MozambiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melvinnunes.mozambique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MozambiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
