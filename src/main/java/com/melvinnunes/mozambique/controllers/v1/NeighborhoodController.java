863b4d2de703eea4b00a45ff345c02ee07d531df
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-21 01:05:25 +0200

    refactor(controllers): rename and update locality-related controllers
    - added NeighborhoodController
    - added VillagesController
    - Update endpoints, request parameters, and descriptions
    - Remove unused import of ProvinceDTO

---
+++ src/main/java/com/melvinnunes/mozambique/controllers/v1/NeighborhoodController.java
@@ -0,0 +1,37 @@
+package com.melvinnunes.mozambique.controllers.v1;
+
+import com.melvinnunes.mozambique.domain.services.CountryService;
+import com.melvinnunes.mozambique.response.ApiResponse;
+import com.melvinnunes.mozambique.response.DefaultItemDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "Neighborhoods", description = "Neighborhoods collections")
+@RestController
+@RequestMapping("/v1/neighborhoods")
+public class NeighborhoodController {
+    private final CountryService countryService;
+
+    public NeighborhoodController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @Operation(description = "List of neighborhoods of locality")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<DefaultItemDTO>>> list(
+            @RequestParam(name = "localityCode") String code
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "List of neighborhoods",
+                countryService.listNeighborhoods(code)
+        ));
+    }
+}
---
+++ src/main/java/com/melvinnunes/mozambique/controllers/v1/VillagesController.java
@@ -0,0 +1,37 @@
+package com.melvinnunes.mozambique.controllers.v1;
+
+import com.melvinnunes.mozambique.domain.services.CountryService;
+import com.melvinnunes.mozambique.response.ApiResponse;
+import com.melvinnunes.mozambique.response.DefaultItemDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Tag(name = "Villages", description = "Villages collections")
+@RestController
+@RequestMapping("/v1/villages")
+public class VillagesController {
+    private final CountryService countryService;
+
+    public VillagesController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @Operation(description = "List of villages of neighborhood")
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<DefaultItemDTO>>> list(
+            @RequestParam(name = "neighborhoodCode") String code
+    ) {
+        return ResponseEntity.status(HttpStatus.OK).body(new ApiResponse<>(
+                "List of villages",
+                countryService.listVillages(code)
+        ));
+    }
+}
