195304e8795c1535efc93c8dfe62b92774a43f02
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-21 00:31:09 +0200

    feat(controller): add ProvinceController with base annotations
    This commit adds the initial structure for the ProvinceController class in the v1 API. It includes the following changes:
    
    - Add @RestController annotation to define it as a REST controller
    - Add @RequestMapping annotation to set the base path for province-related endpoints to "/v1/provinces"
    - Import necessary Spring annotations

---
+++ src/main/java/com/melvinnunes/mozambique/controllers/v1/ProvinceController.java
@@ -0,0 +1,10 @@
+package com.melvinnunes.mozambique.controllers.v1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/provinces")
+public class ProvinceController {
+
+}
