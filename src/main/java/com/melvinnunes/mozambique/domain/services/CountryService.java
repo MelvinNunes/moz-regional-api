2344f563efe132104fa6e17169070645fd7c9687
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-20 22:51:32 +0200

    feat(CountryService): add constructor with CountryDataRepo dependency
    - Inject CountryDataRepo as a dependency
    - Prepare for implementing country-related operations

--- src/main/java/com/melvinnunes/mozambique/domain/services/CountryService.java
+++ src/main/java/com/melvinnunes/mozambique/domain/services/CountryService.java
@@ -1,7 +1,15 @@
 package com.melvinnunes.mozambique.domain.services;
 
+import com.melvinnunes.mozambique.domain.repositories.CountryDataRepo;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
+    private final CountryDataRepo countryDataRepo;
+
+    public CountryService(CountryDataRepo countryDataRepo) {
+        this.countryDataRepo = countryDataRepo;
+    }
 }
