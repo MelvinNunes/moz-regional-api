93c65736eb3fbbf194fe6e5bc8ff379a31ba56ca
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-23 23:38:58 +0200

    feat(swagger): add server configurations to OpenAPI
    - Add local development server URL (http://localhost:8080)
    - Add production server URL (https://moz-api-945722244201.africa-south1.run.app)
    - Update GroupedOpenApi group name from "https" to "http"
    - TODO: Refactor server URLs to use environment variables

--- src/main/java/com/melvinnunes/mozambique/infrastructure/config/SwaggerConfig.java
+++ src/main/java/com/melvinnunes/mozambique/infrastructure/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.melvinnunes.mozambique.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,13 +14,15 @@ public class SwaggerConfig {
         return new OpenAPI().info(new Info()
                 .title("Mozambique Regional Info API Documentation")
                 .description("API Documentation for Mozambique Regional Information")
-                .version("1.0.0"));
+                .version("1.0.0"))
+                .addServersItem(new Server().url("http://localhost:8080").description("Local Development Server")) // this needs refactor to .env
+                .addServersItem(new Server().url("https://moz-api-945722244201.africa-south1.run.app").description("Production Server"));
     }
 
     @Bean
     public GroupedOpenApi httpApi() {
         return GroupedOpenApi.builder()
-                .group("https")
+                .group("http")
                 .pathsToMatch("/**")
                 .build();
     }
