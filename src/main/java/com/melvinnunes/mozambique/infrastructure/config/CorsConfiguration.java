fa11f9c14c1f845e2b97e494ddc4de4d8af2f805
Melvin Nunes <melvinnunes14@gmail.com>
2025-01-23 23:31:49 +0200

    refactor(config): update package name and import in CorsConfiguration

---
+++ src/main/java/com/melvinnunes/mozambique/infrastructure/config/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.melvinnunes.mozambique.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*");
+            }};
+    }
+}
